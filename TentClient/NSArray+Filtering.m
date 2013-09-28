3c28962b5cc91a93ece02dc48ad629ea695d2eaa
Jesse Stuart <jesse@jessestuart.ca>
2013-09-28 13:38:05 -0400

    Update license

--- TentClient/NSArray+Filtering.m
+++ TentClient/NSArray+Filtering.m
@@ -4,6 +4,7 @@
 //
 //  Created by Jesse Stuart on 9/27/13.
 //  Copyright (c) 2013 Tent.is, LLC. All rights reserved.
+//  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 //
 
 #import "NSArray+Filtering.h"
