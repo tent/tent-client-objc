13a83b64936e7535d76b44bf9d835cdd9467efb0
Jesse Stuart <jesse@jessestuart.ca>
2013-10-01 20:45:22 -0400

    Fix copyright in file headers

--- TentClient/NSURL+QueryStringEncoding.h
+++ TentClient/NSURL+QueryStringEncoding.h
@@ -3,7 +3,8 @@
 //  TentClient
 //
 //  Created by Jesse Stuart on 10/1/13.
-//  Copyright (c) 2013 Tent. All rights reserved.
+//  Copyright (c) 2013 Tent.is, LLC. All rights reserved.
+//  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 //
 
 #import <Foundation/Foundation.h>
