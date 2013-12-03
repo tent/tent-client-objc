472f112b7b74eea3b72a820036780818671414b3
Jesse Stuart <jesse@jessestuart.ca>
2013-12-03 12:01:50 -0500

    Format code

--- Classes/Extensions/NSArray+Filtering.m
+++ Classes/Extensions/NSArray+Filtering.m
@@ -4,33 +4,35 @@
 //
 //  Created by Jesse Stuart on 9/27/13.
 //  Copyright (c) 2013 Tent.is, LLC. All rights reserved.
-//  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+//  Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
 //
 
 #import "NSArray+Filtering.h"
 
 @implementation NSArray (Filtering)
 
-- (NSArray *)filteredArrayUsingKeepBlock:(BOOL (^)(id))keepBlock valueBlock:(id (^)(id))valueBlock {
-    NSMutableArray *tmpArray = [[NSMutableArray alloc] init];
+- (NSArray *)filteredArrayUsingKeepBlock:(BOOL (^)(id))keepBlock
+                              valueBlock:(id (^)(id))valueBlock {
+  NSMutableArray *tmpArray = [[NSMutableArray alloc] init];
 
-    for (id obj in self) {
-        if (keepBlock(obj)) {
-            [tmpArray addObject:valueBlock(obj)];
-        }
+  for (id obj in self) {
+    if (keepBlock(obj)) {
+      [tmpArray addObject:valueBlock(obj)];
     }
+  }
 
-    return [NSArray arrayWithArray:tmpArray];
+  return [NSArray arrayWithArray:tmpArray];
 }
 
 - (NSArray *)transposedArrayUsingBlock:(id (^)(id))block {
-    NSMutableArray *tmpArray = [[NSMutableArray alloc] init];
+  NSMutableArray *tmpArray = [[NSMutableArray alloc] init];
 
-    for (id obj in self) {
-        [tmpArray addObject:block(obj)];
-    }
+  for (id obj in self) {
+    [tmpArray addObject:block(obj)];
+  }
 
-    return [NSArray arrayWithArray:tmpArray];
+  return [NSArray arrayWithArray:tmpArray];
 }
 
 @end
--- Classes/TCLink.h
+++ Classes/TCLink.h
@@ -4,16 +4,17 @@
 //
 //  Created by Jesse Stuart on 10/6/13.
 //  Copyright (c) 2013 Tent.is, LLC. All rights reserved.
-//  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+//  Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface TCLink : NSObject
 
-@property (nonatomic) NSURL *URL;
+@property(nonatomic) NSURL *URL;
 
-@property (nonatomic) NSDictionary *attributes;
+@property(nonatomic) NSDictionary *attributes;
 
 + (NSArray *)parseHTMLLinkTagsFromHTML:(NSString *)htmlString;
 
